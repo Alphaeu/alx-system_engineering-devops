6960b511f50c02457e9896a0d3deba06b0ce030f
Alphaeu <dealphaeus@gmail.com>
2024-01-24 13:42:07 -0800

    Updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# Install flask
-package { 'flask':
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Ensure the package is installed
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Notify the user about the package installation
+notify { 'Flask installed successfully':
+  require => Package['Flask'],
 }
